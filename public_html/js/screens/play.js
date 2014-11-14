6f35d0e1dca98f6189b41665163b32f2d88af19e
Shamon Fowler <shamonlfjr@gmail.com>
2014-11-14 11:10:59 -0800

    no message

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -6,7 +6,7 @@ game.PlayScreen = me.ScreenObject.extend({
 		// reset the score
 		game.data.score = 0;
                 
-                me.levelDirector.loadLevel("ShamonLevel01")
+                me.levelDirector.loadLevel("ShamonLevel01");
                 
                 var player = me.pool.pull("Mario", 0, 420, {});
                 me.game.world.addChild(player, 3);
